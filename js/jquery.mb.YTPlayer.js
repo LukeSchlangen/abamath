425678b55d75b315df965b5e2caa0072f674d741
LukeSchlangen <lukeschlangen@gmail.com>
2015-01-15 09:21:29 -0600

    3000?

--- js/jquery.mb.YTPlayer.js
+++ js/jquery.mb.YTPlayer.js
@@ -26,7 +26,7 @@ function onYouTubePlayerAPIReady() {
         return;
 
     ytp.YTAPIReady=true;
-    jQuery(document).trigger("YTAPIReady").delay(1000);
+    jQuery(document).trigger("YTAPIReady").delay(3000);
 }
 
 (function (jQuery, ytp) {
