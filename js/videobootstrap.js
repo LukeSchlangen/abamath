0240fa4508b7769bdea2058577074a54cfddaec9
lukeschlangen <lukeschlangen@gmail.com>
2015-02-08 15:02:40 -0600

    top nav collapse timing implementation

--- js/videobootstrap.js
+++ js/videobootstrap.js
@@ -72,10 +72,10 @@ $(document).ready(function () {
 
 // jQuery to collapse the navbar on scroll
 $(window).scroll(function() {
-    if ($(".navbar").offset().top > 50) {
-        $(".navbar-fixed-top").addClass("top-nav-collapse");  
+    if ($(".navbar").offset().top < 50) {
+        $(".navbar-fixed-top").removeClass("top-nav-collapse");  
     } else {
-        $(".navbar-fixed-top").removeClass("top-nav-collapse");
+        $(".navbar-fixed-top").addClass("top-nav-collapse");
     }
 });
 
